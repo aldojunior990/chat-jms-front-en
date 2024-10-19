bf9cb8193e206612a868986a722e624b6061a4e1
aldojunior990 <aldojunior990@gmail.com>
2024-10-19 16:30:51 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { ConnectionForm } from "@/components/connection-form";
 
 export default function Home() {
   return (
-    <div className="w-full h-screen flex flex-col justify-center items-center bg-background text-primary">
+    <div className="w-full h-screen flex flex-col justify-center items-center  bg-background text-primary">
       <ConnectionForm />
     </div>
   );
--- src/components/connection-form.tsx
+++ src/components/connection-form.tsx
@@ -3,14 +3,14 @@ export function ConnectionForm() {
     <form className="flex w-full justify-center gap-1 items-center" action="">
       <input
         type="text"
-        placeholder="Insira seu nome de usuario border"
+        placeholder="Insira seu nome de usuario"
         name=""
         id=""
-        className="px-4 h-12"
+        className="px-4 h-12 border"
       />
       <input
         type="submit"
-        className="h-12 px-4 rounded-sm bg-primary text-white"
+        className="h-12 px-4 rounded-sm bg-primary text-white cursor-pointer"
         value="Conectar"
         name=""
         id=""
